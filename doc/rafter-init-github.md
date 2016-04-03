bdcedfeed5eabe78db1b52984be0a4d12690506e
Nick Harris <nharris85@gmail.com>
2016-04-03 13:41:31 +0100

    Added manpage doc generation

---
+++ doc/rafter-init-github.md
@@ -0,0 +1,8 @@
+# Rafter - Git Repository Manager
+## DESCRIPTION
+Initializes the local Git Repository Manager
+## OPTIONS
+## EXAMPLES
+```bash
+rafter init github
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,69 @@
-(function(){
-    var gulp = require('gulp'),
-        mocha = require('gulp-mocha'),
-        jshint = require('gulp-jshint'),
-        gutil = require('gulp-util'),
-        lodash = require('lodash'),
-        runSequence = require('run-sequence'),
-        istanbul = require('gulp-istanbul'),
-        del = require('del'),
-        paths = {
-            scripts: ['commands/**/*.js'],
-            tests: ['test/**/*.spec.js']
-        };
+(function() {
+  var gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    jshint = require('gulp-jshint'),
+    gutil = require('gulp-util'),
+    lodash = require('lodash'),
+    runSequence = require('run-sequence'),
+    istanbul = require('gulp-istanbul'),
+    del = require('del'),
+    paths = {
+      scripts: ['commands/**/*.js'],
+      tests: ['test/**/*.spec.js']
+    };
 
-    // Test Tasks
-    gulp.task('mocha', function(){
-        global.expect = require('chai').expect;
-        global.assert = require('chai').assert;
-        global.sinon = require('sinon');
-        global.path = require('path');
-        require('chai').use(require('chai-as-promised'));
-        require('chai').use(require('sinon-chai'));
-        require('sinon-as-promised');
+  // Test Tasks
+  gulp.task('mocha', function() {
+    global.expect = require('chai').expect;
+    global.assert = require('chai').assert;
+    global.sinon = require('sinon');
+    global.path = require('path');
+    require('chai').use(require('chai-as-promised'));
+    require('chai').use(require('sinon-chai'));
+    require('sinon-as-promised');
 
+    return gulp.src(paths.tests, {
+        read: false
+      })
+      .pipe(mocha({
+        reporter: 'spec',
+        should: require('chai').should()
+      }));
+  });
 
+  gulp.task('pre-test', function() {
+    return gulp.src(paths.scripts)
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire());
+  });
 
-        return gulp.src(paths.tests, {read: false})
-            .pipe(mocha({
-                reporter: 'spec',
-                should: require('chai').should()
-            }));
-    });
+  gulp.task('code-coverage', function() {
+    return gulp.src(paths.tests)
+      .pipe(istanbul.writeReports({
+        dir: 'dist/reports/coverage'
+      }))
+      .pipe(istanbul.enforceThresholds({
+        thresholds: {
+          global: {
+            branches: 75
+          }
+        }
+      }));
+  })
 
-    gulp.task('pre-test', function(){
-      return gulp.src(paths.scripts)
-        .pipe(istanbul())
-        .pipe(istanbul.hookRequire());
-    });
+  gulp.task('lint', function() {
+    return gulp.src(paths.scripts.concat(paths.tests))
+      .pipe(jshint())
+      .pipe(jshint.reporter('jshint-stylish'));
+  });
 
-    gulp.task('code-coverage', function(){
-      return gulp.src(paths.tests)
-        .pipe(istanbul.writeReports({
-          dir: 'dist/reports/coverage'
-        }))
-        .pipe(istanbul.enforceThresholds(
-          {
-            thresholds: {
-              global: {
-                branches: 75
-              }
-            }
-          }));
-    })
+  gulp.task('test', function(done) {
+    runSequence('lint', 'pre-test', 'mocha', 'code-coverage', done);
+  });
 
-    gulp.task('lint', function(){
-        return gulp.src(paths.scripts.concat(paths.tests))
-            .pipe(jshint())
-            .pipe(jshint.reporter('jshint-stylish'));
-    });
+  gulp.task('default', ['test']);
 
-    gulp.task('test', function(done){
-      runSequence('lint', 'pre-test', 'mocha', 'code-coverage', done);
-    });
-
-    gulp.task('default', ['test']);
-
-    gulp.task('clean', function(done){
-      del(['dist/*'], done)
-    });
+  gulp.task('clean', function(done) {
+    del(['dist/*'], done)
+  });
 })();
